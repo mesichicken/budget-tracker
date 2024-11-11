3187fb2c65a766a868516a499d7c1c680ec825ec
mesi <mesicharida202@gmail.com>
2024-11-11 22:33:51 +0900

    Refactor Cargo.toml and main.rs to add csv dependency and handle new command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
+use csv::Writer;
 #[derive(Parser)]
 #[clap(version = "1.0")]
 struct App {
@@ -9,7 +10,7 @@ struct App {
 #[derive(Subcommand)]
 enum Command {
     /// 新しい口座を作る
-    New,
+    New(NewArgs),
     /// 口座に入金する
     Deposit,
     /// 口座から出金する
@@ -20,17 +21,27 @@ enum Command {
     Report,
 }
 
+#[derive(Args)]
+struct NewArgs {
+    account_name: String,
+}
+
+impl NewArgs {
+    fn run(&self) {
+        let file_name = format!("{}.csv", self.account_name);
+        let mut writer = Writer::from_path(file_name).unwrap();
+        writer.write_record(["日付", "用途", "全額"]).unwrap();
+        writer.flush().unwrap();
+    }
+}
+
 fn main() {
     let args = App::parse();
     match args.command {
-        Command::New => new(),
+        Command::New(args) => args.run(),
         Command::Deposit => unimplemented!(),
         Command::Withdraw => unimplemented!(),
         Command::Import => unimplemented!(),
         Command::Report => unimplemented!(),
     }
-}
-
-fn new() {
-    println!("New command");
 }
